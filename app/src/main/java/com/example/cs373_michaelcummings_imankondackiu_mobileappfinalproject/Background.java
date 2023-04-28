892a9d13d4ee3fc1017f507a98065156808d0b34
Michael Cummings <mcummings7@worcester.edu>
2023-04-28 18:03:55 -0400

    13th commit, rewrote Background to be a MovableGameObject, still can't get the game to launch. I just want to see the background at least!

--- app/src/main/java/com/example/cs373_michaelcummings_imankondackiu_mobileappfinalproject/Background.java
+++ app/src/main/java/com/example/cs373_michaelcummings_imankondackiu_mobileappfinalproject/Background.java
@@ -3,15 +3,37 @@ package com.example.cs373_michaelcummings_imankondackiu_mobileappfinalproject;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Build;
 
-public class Background {
+import androidx.annotation.RequiresApi;
+
+@RequiresApi(api = Build.VERSION_CODES.O)
+public class Background extends MovableGameObject{
     int x = 0, y = 0;
-    Bitmap background;
+    String backgroundName = "Background";
+    boolean backgroundNoCollide = true;
+    Color backgroundColor = Color.valueOf(Color.GRAY);
+    final int backgroundSpeed = 0;
+    Paint backgroundPaint;
+    Bitmap backgroundBitmap;
+    float backgroundWidth = 1080;
+    float backgroundHeight = 1920;
+    Resources backgroundResources;
 
-    Background(int screenX, int screenY, Resources res){
 
-        background = BitmapFactory.decodeResource(res, R.color.gray);
-        background = Bitmap.createScaledBitmap(background, screenX, screenY, false);
+    Background(){
+        this.setGameObjectName(backgroundName);
+        this.setGameObjectLocation(x, y);
+        this.setGameObjectColor(backgroundColor);
+        this.backgroundPaint.setColor(backgroundColor.toArgb());
+        this.setGameObjectWidth(backgroundWidth);
+        this.setGameObjectHeight(backgroundHeight);
+        this.setMovableGameObjectSpeed(backgroundSpeed);
+        this.setGameObjectNoCollide(backgroundNoCollide);
+        this.backgroundBitmap = BitmapFactory.decodeResource(backgroundResources, R.color.gray);
+        this.backgroundBitmap = Bitmap.createScaledBitmap(backgroundBitmap, x, y, false);
     }
 
 }
